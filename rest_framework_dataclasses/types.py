27cd263223e5ffb9db4b08fef875d8a1351843bc
Oxan van Leeuwen <oxan@oxanvanleeuwen.nl>
2022-11-10 21:12:04 +0100

    Fix Dataclass typehint for mypy 0.990+ (fixes #64)

--- rest_framework_dataclasses/types.py
+++ rest_framework_dataclasses/types.py
@@ -1,6 +1,6 @@
 # Some type definitions that can be useful.
 
-from typing import Dict
+from typing import ClassVar, Dict, Union
 
 try:
     # Python 3.8 and later
@@ -9,6 +9,14 @@ except ImportError:
     from typing_extensions import Final, Literal, Protocol
 
 
-# Note that this doesn't actually work yet (https://stackoverflow.com/a/55240861)
-class Dataclass(Protocol):
-    __dataclass_fields__: Dict
+# As dataclasses don't have a baseclass, we typehint them using a protocol matching the injected `__dataclass_fields__`
+# attribute. Mypy 0.990 changed the attribute from an instance variable to a ClassVar, so use an union for compatibility
+# with both older and newer versions. For reference, see https://stackoverflow.com/a/55240861 and
+# https://github.com/python/mypy/issues/14029.
+class NewStyleDataclassProtocol(Protocol):
+    __dataclass_fields__: ClassVar[dict]
+
+class OldStyleDataclassProtocol(Protocol):
+    __dataclass_fields__: dict
+
+Dataclass = Union[OldStyleDataclassProtocol, NewStyleDataclassProtocol]
